a20821d118deacf43f6ab133a997142f532547e0
zxy <zhangxycg@126.com>
2019-12-26 16:14:02 +0800

    创建教师管理项目--创建父工程和子模块

---
+++ zxy-eduservice/src/main/java/com/zxy/edu/eduservice/EduServiceApplication.java
@@ -0,0 +1,17 @@
+package com.zxy.edu.eduservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: 启动类
+ * @Author: zhangxy
+ * @Date: Created in 2019/12/26
+ */
+@SpringBootApplication
+public class EduServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduServiceApplication.class,args);
+    }
+}
